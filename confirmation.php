1ed0e7a8f4bdb1f8eb4a619b5f1b6995955405e0
unknown <alice.evariste@gmail.com>
2021-07-27 19:33:09 +0200

    integration sql page de confirmation

--- confirmation.php
+++ confirmation.php
@@ -12,24 +12,56 @@
     <section>
         <?php 
         // teste si il y a un formulaire
-        if isset($_POST)
+        if (isset($_POST["username"]))
+        {
             //essaie une connexion au SQL et traite l'exception
-            {
             try
             {
-                $bdd = new PDO('mysql:host=localhost;dbname=gbaf;charset=utf8', 'root', 'root');
+                $bdd = new PDO('mysql:host=localhost;dbname=gbaf;charset=utf8', 
+                'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             }
             catch (exception $e)
             {
                 die('Erreur : ' . $e->getMessage());
             }
-            //la demande SQL
-            $sql_query = "SELECT COUNT(*) AS num FROM 'account' WHERE username = :username";
-            //prépare le SQL
-            $stmt = $bdd->prepare($sql_query);
+            // vérifie username
+            $req = $bdd->prepare("SELECT username FROM account WHERE username = ?");
+            $req->execute(array($_POST["username"]));
+            $user = $req->fetch();
+            if ($_POST['username'] == $user['username'])
+            {
+                ?>
+                <p>
+                    Il y a déjà un utilisateur à ce nom.
+                </p>
+                <?php
+            }
+            else
+            // ajoute l'entrée dans "account"
+            {
+                $ins = $bdd->prepare("INSERT INTO account(nom, prenom, username, password, question, reponse)
+                VALUES (:nom, :prenom, :username, :password, :question, :reponse)");
+                $ins->exec(array(
+                    'nom' => $_POST['nom'], 
+                    'prenom' => $_POST['prenom'], 
+                    'username' => $_POST['username'], 
+                    'password' => $_POST['password'], 
+                    'question' => $_POST['question'], 
+                    'reponse' => $_POST['reponse']
+                ));
+            }
             
-            if $_POST['username'] == 
-
+            //$req->closeCursor();
+        }   
+        else
+        {
+        ?>
+        <p>
+            Redirection vers la page d'enregistrement...
+        </p>
+        <?php
+        }
+        ?>
     </section>
     <?php include('footer.php'); ?>
 </body>
